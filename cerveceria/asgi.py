cd016a10b34354ad64f530a9368a4a8ba08fb504
Rodrigo <canterorodrigo999@gmail.com>
2025-03-09 21:18:06 -0300

    Agregar configuración de PgBouncer

--- cerveceria/asgi.py
+++ cerveceria/asgi.py
@@ -1,16 +1,24 @@
-"""
-ASGI config for cerveceria project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
 
+# Configura el módulo de configuración de Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cerveceria.settings')
 
-application = get_asgi_application()
+# Aplicación ASGI para HTTP
+django_application = get_asgi_application()
+
+# Si usas WebSockets, agrega la configuración de Channels aquí.
+# Por ejemplo:
+# from myapp import consumers
+
+application = ProtocolTypeRouter({
+    "http": django_application,  # Maneja solicitudes HTTP
+    # "websocket": AuthMiddlewareStack(  # Maneja WebSockets (opcional)
+    #     URLRouter([
+    #         path("ws/some_path/", consumers.MyConsumer.as_asgi()),
+    #     ])
+    # ),
+})
