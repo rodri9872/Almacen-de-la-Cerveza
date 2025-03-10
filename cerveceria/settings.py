3b24a10c2686190a6813693d76f5023a9204dbbc
Rodrigo <canterorodrigo999@gmail.com>
2025-03-09 21:28:24 -0300

    Agregar configuración de PgBouncer

--- cerveceria/settings.py
+++ cerveceria/settings.py
@@ -138,6 +138,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
